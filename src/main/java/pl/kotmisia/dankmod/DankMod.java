5f43fa3dbeb53a043d7063e787ccb2990a4268a8
Mm2PL <miau@mail.kotmisia.pl>
2022-03-03 23:59:11 +0100

    reindent

--- src/main/java/pl/kotmisia/dankmod/DankMod.java
+++ src/main/java/pl/kotmisia/dankmod/DankMod.java
@@ -10,29 +10,23 @@ import org.slf4j.LoggerFactory;
 import pl.kotmisia.dankmod.config.DankCfg;
 
 public class DankMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("dankmod");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("dankmod");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-			dispatcher.register(CommandManager.literal("test").executes(context -> {
-				LOGGER.warn(context.getInput());
-				return 0;
-			}));
-		});
-		AutoConfig.register(DankCfg.class, GsonConfigSerializer::new);
-	}
+        LOGGER.info("Hello Fabric world!");
+        AutoConfig.register(DankCfg.class, GsonConfigSerializer::new);
+    }
 
-	public static DankCfg getConfig() {
-		return AutoConfig.getConfigHolder(DankCfg.class).getConfig();
-	}
+    public static DankCfg getConfig() {
+        return AutoConfig.getConfigHolder(DankCfg.class).getConfig();
+    }
 
 }
